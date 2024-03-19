d4a1d03458c01a5bc95f7ffe9cf52cfb0f2fb90a
Soorya7 <105735689+Soorya7@users.noreply.github.com>
2024-03-19 21:15:16 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -1,14 +1,10 @@
-package com.example.explicitintent;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+}
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public void newsScreen(View view) {
+    Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+    startActivity(i);
 }
